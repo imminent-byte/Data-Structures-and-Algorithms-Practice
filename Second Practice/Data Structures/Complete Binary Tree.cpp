f38bf67021b42f0828ca1cd8c9c1ed863a59bc5e
imminent-byte <124569958+imminent-byte@users.noreply.github.com>
2023-08-24 15:19:01 +0300

    Create Complete Binary Tree.cpp

---
+++ Second Practice/Data Structures/Complete Binary Tree.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+struct Node{
+	int root;
+	struct Node* left, * right;
+};
+
+struct Node* newNode(char k) {
+	struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+	node->root = k;
+	node->left = node->right = NULL;
+	return node;
+}
+
+int countNumNodes(struct Node* root) {
+	if (root == NULL)
+		return (0);
+	return (1 + countNumNodes(root->left) + countNumNodes(root->right));
+}
+
+bool checkComplete(struct Node* root, int index, int numberNodes) {
+	if (root == NULL)
+		return true;
+
+	if (index >= numberNodes)
+		return false;
+
+	return (checkComplete(root->left, 2 * index + 1, numberNodes) && checkComplete(root->right, 2 * index + 2, numberNodes));
+}
+
+int main() {
+	struct Node* root = NULL;
+	root = newNode(1);
+	root->left = newNode(2);
+	root->right = newNode(3);
+	root->left->left = newNode(4);
+	root->left->right = newNode(5);
+	root->right->left = newNode(6);
+
+	int node_count = countNumNodes(root);
+	int index = 0;
+
+	if (checkComplete(root, index, node_count))
+		cout << "Binary Tree\n";
+	else
+		cout << "Not Binary Tree\n";
+}
