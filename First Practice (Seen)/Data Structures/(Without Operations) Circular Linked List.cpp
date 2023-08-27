47a2ebd47e25cefa99207a82972672146b32a07c
imminent-byte <124569958+imminent-byte@users.noreply.github.com>
2023-08-27 18:23:30 +0300

    Create (Without Operations) Circular Linked List.cpp

---
+++ First Practice (Seen)/Data Structures/(Without Operations) Circular Linked List.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+	int data;
+	Node* next;
+	Node* prev;
+};
+
+typedef struct Node node;
+
+int main() {
+	node* HEAD;
+	node* one = NULL;
+	node* four = NULL;
+	node* two = NULL;
+	node* three = NULL;
+
+	one = (node*)malloc(sizeof(node));
+	four = (node*)malloc(sizeof(node));
+	two = (node*)malloc(sizeof(node));
+	three = (node*)malloc(sizeof(node));
+
+	one->data = 1;
+	four->data = 4;
+	two->data = 2;
+	three->data = 3;
+
+	one->next = two;
+	two->next = three;
+	three->next = one;
+
+	HEAD = one;
+
+	while (HEAD != NULL) {
+		cout << HEAD->data << " ";
+		HEAD = HEAD->next;
+	}
+}
