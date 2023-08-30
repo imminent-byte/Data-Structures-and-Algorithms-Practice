b3f5fccc75c61870819359d4e19b8f2d91099046
imminent-byte <124569958+imminent-byte@users.noreply.github.com>
2023-08-30 21:49:40 +0300

    Create (With Operations)Circular Linked List.cpp

---
+++ First Practice (Seen)/Data Structures/(With Operations)Circular Linked List.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+using namespace std;
+
+class node {
+public:
+	int data;
+	node* next;
+
+	node(int val) {
+		data = val;
+		next = NULL;
+	}
+};
+
+void insertAtBeginning(node*& head, int data) {
+	node* newNode = new node(data);
+
+	if (head == NULL) {
+		newNode->next = newNode;
+		head = newNode;
+		return;
+	}
+
+	node* temp = head;
+
+	while (temp->next != head) {
+		temp = temp->next;
+	}
+	
+	temp->next = newNode;
+	newNode->next = head;
+	head = newNode;
+}
+
+void insertAtEnd(node*& head, int data) {
+	if (head == NULL) {
+		insertAtBeginning(head, data);
+		return;
+	}
+
+	node* newNode = new node(data);
+	node* temp = head;
+
+	while (temp->next != head) temp = temp->next;
+
+	temp->next = newNode;
+	newNode->next = head;
+}
+
+void deleteAtHead(node*& head) {
+	node* temp = head;
+	while (temp->next != head) {
+		temp = temp->next;
+	}
+
+	node* todelete = head;
+	temp->next = head->next;
+	head = head->next;
+
+	delete todelete;
+}
+
+void deletion(node*& head, int pos) {
+	if (pos == 1) {
+		deleteAtHead(head);
+		return;
+	}
+
+	node* temp = head;
+	int count = 1;
+
+	while (count != pos - 1) {
+		temp = temp->next;
+		count++;
+	}
+
+	node* todelete = temp->next;
+	temp->next = temp->next->next;
+
+	delete todelete;
+}
+
+void display(node* head) {
+	node* temp = head;
+
+	//Didn't use while loop here because if the condition gets true we wont get to last element
+	do {
+		cout << temp->data << " ";
+		temp = temp->next;
+	} while (temp != head);
+	cout << endl;
+}
+
+int main() {
+	node* head = NULL;
+	insertAtEnd(head, 1);
+	insertAtEnd(head, 2);
+	insertAtEnd(head, 3);
+	insertAtEnd(head, 4);
+	display(head);
+	insertAtBeginning(head, 5);
+	display(head);
+	deletion(head, 5);
+	display(head);
+}
