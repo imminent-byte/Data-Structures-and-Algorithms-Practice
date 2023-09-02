1f65c0185ef01905e57219c9d69964f6c3b35a13
imminent-byte <124569958+imminent-byte@users.noreply.github.com>
2023-09-02 20:45:14 +0300

    Create Balanced Binary Tree.cpp

---
+++ First Practice (Seen)/Data Structures/Balanced Binary Tree.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+#define bool int
+
+class node {
+public:
+	int item;
+	node* left;
+	node* right;
+};
+
+node* newNode(int item) {
+	node* Node = new node();
+	Node->item = item;
+	Node->left = NULL;
+	Node->right = NULL;
+
+	return (Node);
+}
+
+bool checkHeightBalace(node* root, int* height) {
+	int leftHeight = 0, rightHeight = 0;
+
+	int l = 0, r = 0;
+
+	if (root == NULL) {
+		*height = 0;
+		return 1;
+	}
+
+	l = checkHeightBalace(root->left, &leftHeight);
+	r = checkHeightBalace(root->right, &rightHeight);
+
+	*height = (leftHeight > rightHeight ? leftHeight : rightHeight) + 1;
+
+	if (std::abs(leftHeight - rightHeight >= 2))
+		return 0;
+
+	else
+		return l && r;
+}
+
+int main() {
+	int height = 0;
+
+	node* root = newNode(1);
+	root->left = newNode(2);
+	root->right = newNode(3);
+	root->left->left = newNode(4);
+	root->left->right = newNode(5);
+
+	if (checkHeightBalace(root, &height))
+		cout << "The tree is balanced";
+	else
+		cout << "The tree is not balanced";
+}
