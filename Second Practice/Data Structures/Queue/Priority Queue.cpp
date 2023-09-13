e25d1b07bb7d1051126c94e4a037bf19638e2bdf
imminent-byte <124569958+imminent-byte@users.noreply.github.com>
2023-09-13 16:13:49 +0300

    Create Priority Queue.cpp

---
+++ Second Practice/Data Structures/Queue/Priority Queue.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void swap(int* a, int* b) {
+    int temp = *b;
+    *b = *a;
+    *a = temp;
+}
+
+void heapify(vector<int>& hT, int i) {
+    int size = hT.size();
+
+    int largest = i;
+    int l = 2 * i + 1;
+    int r = 2 * i + 2;
+    if (l < size && hT[l] > hT[largest])
+        largest = l;
+    if (r < size && hT[r] > hT[largest])
+        largest = r;
+
+    if (largest != i) {
+        swap(&hT[i], &hT[largest]);
+        heapify(hT, largest);
+    }
+}
+
+void Queue(vector<int>& hT, int newNum) {
+    int size = hT.size();
+    if (size == 0) {
+        hT.push_back(newNum);
+    }
+    else {
+        hT.push_back(newNum);
+        for (int i = size / 2 - 1; i >= 0; i--) {
+            heapify(hT, i);
+        }
+    }
+}
+
+void enQueue(vector<int>& hT) {
+    int size = hT.size();
+    swap(&hT[0], &hT[size - 1]);
+
+    hT.pop_back();
+    for (int i = size / 2 - 1; i >= 0; i--) {
+        heapify(hT, i);
+    }
+}
+
+void printArray(vector<int>& hT) {
+    for (int i = 0; i < hT.size(); ++i)
+        cout << hT[i] << " ";
+    cout << "\n";
+}
+
+int main() {
+    vector<int> heapTree;
+
+    Queue(heapTree, 3);
+    Queue(heapTree, 4);
+    Queue(heapTree, 9);
+    Queue(heapTree, 5);
+    Queue(heapTree, 2);
+
+    cout << "Max-Heap array: ";
+    printArray(heapTree);
+
+    enQueue(heapTree);
+
+    enQueue(heapTree);
+
+    cout << "After deleting an element: ";
+
+    printArray(heapTree);
+}
