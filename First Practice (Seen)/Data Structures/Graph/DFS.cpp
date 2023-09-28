fe7c31230cacc55d9ba1969cb01f9fea85039f60
imminent-byte <124569958+imminent-byte@users.noreply.github.com>
2023-09-28 18:46:09 +0300

    Update DFS.cpp

--- First Practice (Seen)/Data Structures/Graph/DFS.cpp
+++ First Practice (Seen)/Data Structures/Graph/DFS.cpp
@@ -28,9 +28,10 @@ Graph::Graph(int vertices) {
 // Add edges
 void Graph::addEdge(int src, int dest) {
 	adjLists[src].push_back(dest);
+	adjLists[dest].push_back(src);
 }
 
-// DFS algorithm
+// DFS algorithm, we dont use stack here because recursion takes care of the stack
 void Graph::DFS(int vertex) {
 	visited[vertex] = true;
 	cout << vertex << '\t';
@@ -41,14 +42,57 @@ void Graph::DFS(int vertex) {
 			DFS(*i);
 }
 
-int main() {
-	Graph g(4);
-	g.addEdge(0, 1);
-	g.addEdge(0, 2);
+void test1() {
+	cout << "//TEST 1//" << endl;
+	Graph g(8);
+	g.addEdge(1, 5);
+	g.addEdge(1, 4);
 	g.addEdge(1, 2);
+	g.addEdge(2, 7);
+	g.addEdge(2, 6);
 	g.addEdge(2, 3);
 
-	g.DFS(0);
+	g.DFS(1);
+	cout << endl;
+}
 
-	return 0;
+void test2() {
+	cout << "//TEST 2//" << endl;
+	Graph g(8);
+	g.addEdge(1, 2);
+	g.addEdge(1, 3);
+	g.addEdge(2, 4);
+	g.addEdge(2, 5);
+	g.addEdge(3, 6);
+	g.addEdge(3, 7);
+
+	g.DFS(1);
+	cout << endl;
+}
+
+void test3() {
+	cout << "//TEST 3//" << endl;
+	Graph g(11);
+	g.addEdge(1, 4);
+	g.addEdge(1, 2);
+	g.addEdge(4, 3);
+	g.addEdge(2, 3);
+	g.addEdge(3, 10);
+	g.addEdge(3, 9);
+	g.addEdge(2, 7);
+	g.addEdge(2, 5);
+	g.addEdge(2, 8);
+	g.addEdge(8, 5);
+	g.addEdge(8, 7);
+	g.addEdge(7, 5);
+	g.addEdge(5, 6);
+
+	g.DFS(1);
+	cout << endl;
+}
+
+int main() {
+	test1();
+	test2();
+	test3();
 }
